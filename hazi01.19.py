aa3c43123987d0b6aa76cfeac6777bdd107eb974
Zalihun69 <159771111+Zalihun69@users.noreply.github.com>
2025-01-19 14:04:31 +0100

    Add files via upload

---
+++ hazi01.19.py
@@ -0,0 +1,11 @@
+def compare_numbers(num1, num2):
+   
+    if num1 > num2:
+        print(f"A nagyobb szám: {num1}")
+    elif num1 < num2:
+        print(f"A nagyobb szám: {num2}")
+    else:
+        print("A két szám egyenlő.")
+
+
+
